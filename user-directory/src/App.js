59d5f612191776501542b0cd5240457728f8cbab
fewb92 <sotojosh96@gmail.com>
2021-07-28 22:07:42 -0400

    created table component

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header'
+import Table from './components/Table'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Header />
+      <Table />
     </div>
   );
 }
